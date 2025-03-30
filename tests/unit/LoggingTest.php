36bc74f323c2c8ba89e946f7c2b5807f88ace224
Dennis Vorst <dennis.vorst@ziggo.nl>
2025-03-30 20:51:18 +0200

    tests added

--- tests/unit/LoggingTest.php
+++ tests/unit/LoggingTest.php
@@ -1,59 +1,72 @@
 <?php
-declare(strict_types=1);
-
 use PHPUnit\Framework\TestCase;
+use App\Models\Logging;
 
 class LoggingTest extends TestCase
 {
-    function testClassLoggingExists()
+    private string $testDir;
+    private string $testFile;
+
+    protected function setUp(): void
     {
-        $this->assertTrue(class_exists("Logging"));
+        $this->testDir = sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'logtest_' . uniqid();
+        $this->testFile = 'test.log';
     }
 
-    public function testClassLoggingCanBeInstatiated()
+    protected function tearDown(): void
     {
-        $object = new Logging();
-        $this->assertInstanceOf(Logging::class, $object);
-    }
-
-    public function testCanLogWarning()
-    {
-        $object = new Logging();
-        $this->assertTrue($object->Warning("test"));
-    }
-
-    public function testCanLogError()
-    {
-        $object = new Logging();
-        $this->assertTrue($object->Error("test"));
-    }
-
-    public function testCanLogInfo()
-    {
-        $object = new Logging();
-        $this->assertTrue($object->Info("test"));
-    }
-
-    public function testCanLogDebug()
-    {
-        $object = new Logging();
-        $this->assertTrue($object->Debug("test"));
-    }
-
-    function __destruct() 
-    {
-        $dir = getcwd() . DIRECTORY_SEPARATOR . "logs" . DIRECTORY_SEPARATOR;
-        $filename = $dir . "logfile.log";
-
-        print_r($filename . "\n");
-        print_r($dir . "\n");
-
-        /** remove the logfile and then the directory */
-        if (file_exists($filename)) {
-            unlink($filename);
-            rmdir($dir);
+        if (file_exists($this->testDir . DIRECTORY_SEPARATOR . $this->testFile)) {
+            unlink($this->testDir . DIRECTORY_SEPARATOR . $this->testFile);
         }
-    }    
+
+        if (is_dir($this->testDir)) {
+            rmdir($this->testDir);
+        }
+    }
+
+    public function testLogFileIsCreated()
+    {
+        $logger = new Logging($this->testDir . DIRECTORY_SEPARATOR, $this->testFile);
+        $fullPath = $this->testDir . DIRECTORY_SEPARATOR . $this->testFile;
+
+        $this->assertFileExists($fullPath);
+        $content = file_get_contents($fullPath);
+        $this->assertStringContainsString('*** Log started at :', $content);
+    }
+
+    public function testErrorLogging()
+    {
+        $logger = new Logging($this->testDir . DIRECTORY_SEPARATOR, $this->testFile);
+        $logger->Error('This is an error');
+        $content = file_get_contents($this->testDir . DIRECTORY_SEPARATOR . $this->testFile);
+//        print_r("content - |" . $content . "|");
+        $this->assertStringContainsString('ERROR   : This is an error', $content);
+    }
+
+    public function testWarningLogging()
+    {
+        $logger = new Logging($this->testDir . DIRECTORY_SEPARATOR, $this->testFile);
+        $logger->Warning('This is a warning');
+        $content = file_get_contents($this->testDir . DIRECTORY_SEPARATOR . $this->testFile);
+        $this->assertStringContainsString('WARNING : This is a warning', $content);
+    }
+
+    public function testInfoLogging()
+    {
+        $logger = new Logging($this->testDir . DIRECTORY_SEPARATOR, $this->testFile);
+        $logger->Info('Some info');
+        $content = file_get_contents($this->testDir . DIRECTORY_SEPARATOR . $this->testFile);
+        $this->assertStringContainsString('INFO    : Some info', $content);
+    }
+
+    public function testDebugLogging()
+    {
+        $logger = new Logging($this->testDir . DIRECTORY_SEPARATOR, $this->testFile);
+        $result = $logger->Debug('Debugging...');
+        $this->assertTrue($result);
+
+        $content = file_get_contents($this->testDir . DIRECTORY_SEPARATOR . $this->testFile);
+        $this->assertStringContainsString('DEBUG   : Debugging...', $content);
+    }
 }
 ?>
-
